de41fa02739c86d6c62895afbad0e8454c00265b
kotars <cat.tch2018@gmail.com>
2022-09-24 18:03:15 +0700

    первый

---
+++ mail.php
@@ -0,0 +1,66 @@
+<?php
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+$json = file_get_contents('php://input');
+$obj = json_decode($json, true);
+
+$name = $obj['user-name'];
+$phone = $obj['user-phone'];
+
+// Формирование самого письма
+$title = "Заголовок письма";
+
+$body = " <h2>Новое письмо</h2>
+  <tr style='background-color: #f8f8f8;'>
+    <td style='padding: 10px; border: #e9e9e9 1px solid;'><b>Имя: </b>$name</td>
+    <td style='padding: 10px; border: #e9e9e9 1px solid;'><b>Номер телефона: </b>$phone</td>
+  </tr>
+  ";
+$body = "<table style='width: 100%;'>$body</table>";
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+
+try {
+  $mail->isSMTP();
+  $mail->CharSet = "UTF-8";
+  $mail->SMTPAuth   = true;
+  // $mail->SMTPDebug = 2; 
+  $mail->Debugoutput = function ($str, $level) {
+    $GLOBALS['status'][] = $str;
+  };
+
+  // Настройки вашей почты
+  $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
+  $mail->Username   = 'cat.tch2018@gmail.com'; // Логин на почте
+  $mail->Password   = 'xvigwgsepgrwuvyt'; // Пароль 
+  $mail->SMTPSecure = 'ssl';
+  $mail->Port       = 465;
+
+  $mail->setFrom('cat.tch2018@gmail.com', 'Дмитрий Чумаков'); // Адрес самой почты и имя отправителя
+
+  // Получатель письма
+  $mail->addAddress('cat.tch2018@gmail.com');
+  // $mail->addAddress('lokl0k@yandex.ru');
+
+  // Отправка сообщения
+  $mail->isHTML(true);
+  $mail->Subject = $title;
+  $mail->Body = $body;
+
+  // Проверяем отравленность сообщения
+  if ($mail->send()) {
+    $result = "success";
+  } else {
+    $result = "error";
+  }
+} catch (Exception $e) {
+  $result = "error";
+  $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+// Отображение результата
+echo json_encode(["result" => $result, "status" => $status]);
